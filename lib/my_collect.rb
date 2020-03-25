250cc054d396546c17e3cc57298ebf66ed8dfb40
Justin Winger <justinwinger4@gmail.com>
2020-03-25 19:09:31 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,10 @@ def my_collect(languages)
   i = 0 
   collection = []
   while i < languages.length 
-    collection << yield[i]
+    collection << yield(languages[i])
+    i += 1 
+  end
+  collection
   
   
 end
